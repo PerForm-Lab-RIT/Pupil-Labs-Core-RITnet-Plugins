e7ed2879a80cccc6db29fe329b2fc75fec7803ad
Kevin Barkevich <kbarkevich@gmail.com>
2021-01-21 15:30:21 -0500

    Initial commit

---
+++ detector_2d_ritnet_pupil_plugin.py
@@ -0,0 +1,281 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..','..','pupil_src','shared_modules','pupil_detector_plugins'))
+from visualizer_2d import draw_pupil_outline
+from pupil_detectors import DetectorBase
+from pupil_detector_plugins.detector_base_plugin import PupilDetectorPlugin
+from pupil_detector_plugins.pye3d_plugin import Pye3DPlugin
+
+import logging
+from methods import normalize
+import torch
+import numpy as np
+from pyglui import ui
+
+from ritnet.image import get_mask_from_PIL_image, init_model, get_pupil_ellipse_from_PIL_image
+from ritnet.models import model_dict, model_channel_dict
+from ritnet.Ellseg.args import parse_precision
+from ritnet.Ellseg.pytorchtools import load_from_file
+
+from ritnet_plugin_settings import ritnet_labels, ritnet_ids, default_id
+
+ritnet_directory = os.path.join(os.path.dirname(__file__), 'ritnet\\')
+filename = "ritnet_pupil.pkl" # best_model.pkl, ritnet_pupil.pkl, ritnet_400400.pkl, ellseg_allvsone
+MODEL_DICT_STR, CHANNELS, IS_ELLSEG, ELLSEG_MODEL = model_channel_dict[filename]
+ELLSEG_FOLDER = 'Ellseg'
+ELLSEG_FILEPATH = ritnet_directory+ELLSEG_FOLDER
+ELLSEG_PRECISION = 32  # precision. 16, 32, 64
+ELLSEG_PRECISION = parse_precision(ELLSEG_PRECISION)
+USEGPU = True
+KEEP_BIGGEST_PUPIL_BLOB_ONLY = True
+
+if USEGPU:
+    device=torch.device("cuda")
+else:
+    device=torch.device("cpu")
+
+logger = logging.getLogger(__name__)
+
+class RITPupilDetector(DetectorBase):
+    def __init__(self, model, model_channels):
+        self._model = model
+        self._model_channels = model_channels
+
+    #def get_circle_coord(self):
+    #    # retreive a new point from the circle on every frame refresh
+    #    coords = self.pnts[self.ind]
+    #    if self.ind < self.stop_ind:
+    #        self.ind += 1
+    #    else:
+    #        self.ind = 0
+    #    return coords
+
+    def detect(self, img):
+        # here we override the detect method with our own custom detector
+        # this is a random artificial pupil center
+        # center = (90, 90)
+        # we move the center around the circle here
+        # center = tuple(sum(x) for x in zip(center, self.get_circle_coord()))
+        # return center
+        return get_pupil_ellipse_from_PIL_image(img, self._model, isEllseg=IS_ELLSEG, ellsegPrecision=ELLSEG_PRECISION)
+
+class Detector2DRITnetPupilPlugin(PupilDetectorPlugin):
+    uniqueness = "by_class"
+    icon_chr = "RP"
+
+    label = "RITnet ritnet_pupil 2d detector"
+    identifier = "ritnet-pupil-2d"
+    order = 0.08
+    
+    @property
+    def pretty_class_name(self):
+        return "RITnet Detector (ritnet_pupil)"
+        
+    @property
+    def pupil_detector(self) -> RITPupilDetector:
+        return self.detector_2d
+        
+    def update_chosen_detector(self, ritnet_unique_id):
+        self.g_pool.ritnet_selection = ritnet_unique_id
+        if ritnet_unique_id == 'ritnet_ellseg':
+            print("---------------------ELLSEG SELECTED---------------------")
+            self.g_pool.plugin_by_name['Detector2DRITnetEllsegAllvonePlugin'] = 0.09
+            self.g_pool.plugin_by_name['Detector2DRITnetBestmodelPlugin'].order = 0.08
+            self.order = 0.08
+        elif ritnet_unique_id == 'ritnet_bestmodel':
+            print("---------------------BESTMODEL SELECTED---------------------")
+            self.g_pool.plugin_by_name['Detector2DRITnetBestmodelPlugin'].order = 0.09
+            self.order = 0.08
+            self.g_pool.plugin_by_name['Detector2DRITnetEllsegAllvonePlugin'] = 0.08
+        elif ritnet_unique_id == 'ritnet_pupil':
+            print("---------------------PUPIL SELECTED---------------------")
+            self.order = 0.09
+            self.g_pool.plugin_by_name['Detector2DRITnetEllsegAllvonePlugin'] = 0.08
+            self.g_pool.plugin_by_name['Detector2DRITnetBestmodelPlugin'].order = 0.08
+        else:
+            print("---------------------NONE SELECTED---------------------")
+            self.g_pool.plugin_by_name['Detector2DRITnetEllsegAllvonePlugin'] = 0.08
+            self.g_pool.plugin_by_name['Detector2DRITnetBestmodelPlugin'].order = 0.08
+            self.order = 0.08
+        
+    def __init__(
+        self,
+        g_pool=None,
+        # properties=None,
+        # detector_2d: Detector2D = None,
+    ):
+        super().__init__(g_pool=g_pool)
+        
+        #  Set up RITnet
+        if torch.cuda.device_count() > 1:
+            print('Moving to a multiGPU setup for Ellseg.')
+            useMultiGPU = True
+        else:
+            useMultiGPU = False
+        model = model_dict[MODEL_DICT_STR]
+    
+        if not IS_ELLSEG:
+            model  = model.to(device)
+            model.load_state_dict(torch.load(ritnet_directory+filename))
+            model = model.to(device)
+            model.eval()
+        else:
+            LOGDIR = os.path.join(ELLSEG_FILEPATH, 'ExpData', 'logs',\
+                              'ritnet_v2', ELLSEG_MODEL)
+            path2checkpoint = os.path.join(LOGDIR, 'checkpoints')
+            checkpointfile = os.path.join(path2checkpoint, 'checkpoint.pt')
+            print(checkpointfile)
+            netDict = load_from_file([checkpointfile, ''])
+            #print(checkpointfile)
+            #print(netDict)
+            model.load_state_dict(netDict['state_dict'])
+            #print('Parameters: {}'.format(get_nparams(model)))
+            model = model if not useMultiGPU else torch.nn.DataParallel(model)
+            model = model.to(device)
+            model = model.to(ELLSEG_PRECISION)
+            model.eval()
+        
+        #  Initialize model
+        
+        self.model = model
+        self.detector_2d = RITPupilDetector(model, 4)
+        self.g_pool.ritnet_pupil_enable = True
+        
+        #self._stop_other_pupil_detectors()  # STOP DETECTORS BESIDES THIS ONE
+    
+    def _stop_other_pupil_detectors(self):
+        plugin_list = self.g_pool.plugins
+
+        # Deactivate other PupilDetectorPlugin instances
+        for plugin in plugin_list:
+            if isinstance(plugin, PupilDetectorPlugin) and plugin is not self and not isinstance(plugin, Pye3DPlugin):
+                plugin.alive = False
+
+        # Force Plugin_List to remove deactivated plugins
+        plugin_list.clean()
+    
+    def detect(self, frame, **kwargs):
+        result = {}
+        ellipse = {}
+        eye_id = self.g_pool.eye_id
+        result["id"] = eye_id
+        result["topic"] = f"pupil.{eye_id}.{self.identifier}"
+        ellipse["center"] = (0.0, 0.0)
+        ellipse["axes"] = (0.0, 0.0)
+        ellipse["angle"] = 0.0
+        result["ellipse"] = ellipse
+        result["diameter"] = 0.0
+        result["location"] = ellipse["center"]
+        result["confidence"] = 0.0
+        result["timestamp"] = frame.timestamp
+        
+        if not self.g_pool.ritnet_pupil_enable:
+            return result
+            
+        img = frame.gray
+        
+        # pred_img, predict = get_mask_from_PIL_image(frame, self.model, USEGPU, False, True, CHANNELS, KEEP_BIGGEST_PUPIL_BLOB_ONLY, isEllseg=IS_ELLSEG, ellsegPrecision=ELLSEG_PRECISION)
+        # ellipsedata = get_pupil_ellipse_from_PIL_image(img, self.model)
+        # img = np.uint8(get_mask_from_PIL_image(img, self.model) * 255)
+        
+        ellipsedata = self.detector_2d.detect(img)
+        
+        if ellipsedata is not None:
+            eye_id = self.g_pool.eye_id
+            result["id"] = eye_id
+            result["topic"] = f"pupil.{eye_id}.{self.identifier}"
+            ellipse["center"] = (ellipsedata[0], ellipsedata[1])
+            ellipse["axes"] = (ellipsedata[2]*2, ellipsedata[3]*2)
+            ellipse["angle"] = ellipsedata[4]
+            result["ellipse"] = ellipse
+            result["diameter"] = ellipsedata[2]*2
+            result["location"] = ellipse["center"]
+            result["confidence"] = 0.99
+            result["timestamp"] = frame.timestamp
+        else:
+            return result
+
+        #eye_id = self.g_pool.eye_id
+        location = result["location"]
+        norm_pos = normalize(
+            location, (frame.width, frame.height), flip_y=True
+        )
+        result["norm_pos"] = norm_pos
+        result["method"] = "2d c++"
+        #result["timestamp"] = frame.timestamp
+        #result["topic"] = f"pupil.{eye_id}.{self.identifier}"
+        #result["id"] = eye_id
+        #result["method"] = "2d c++"
+        #result["previous_detection_results"] = result.copy()
+       
+        return result
+        
+    def gl_display(self):
+        if self._recent_detection_result:
+            draw_pupil_outline(self._recent_detection_result, color_rgb=(0, 1, 0))
+    
+    def init_ui(self):
+        super().init_ui()
+        self.menu.label = self.pretty_class_name
+        self.menu_icon.label_font = "pupil_icons"
+        info = ui.Info_Text(
+            "(GREEN) Model using RITnet, the \"ritnet_pupil\" model."
+        )
+        self.menu.append(info)
+        self.menu.append(
+            ui.Selector(
+                "ritnet_selection",
+                self.g_pool,
+                label="RITnet Selection",
+                selection=ritnet_ids,
+                labels=ritnet_labels,
+                setter=self.update_chosen_detector,
+            )
+        )
+        self.menu.append(
+            ui.Switch(
+                "ritnet_pupil_enable",
+                self.g_pool,
+                label="Enable RITnet bestmodel"
+            )
+        )
+        """
+        self.menu.append(
+            ui.Slider(
+                "2d.intensity_range",
+                self.proxy,
+                label="Pupil intensity range",
+                min=0,
+                max=60,
+                step=1,
+            )
+        )
+        self.menu.append(
+            ui.Slider(
+                "2d.pupil_size_min",
+                self.proxy,
+                label="Pupil min",
+                min=1,
+                max=250,
+                step=1,
+            )
+        )
+        self.menu.append(
+            ui.Slider(
+                "2d.pupil_size_max",
+                self.proxy,
+                label="Pupil max",
+                min=50,
+                max=400,
+                step=1,
+            )
+        )
+        self.menu.append(
+            ui.Switch(
+                "ritnet_2d",
+                self.g_pool,
+                label="Enable RITnet"
+            )
+        )
+        """
---
+++ ritnet_plugin_settings.py
@@ -0,0 +1,3 @@
+ritnet_labels = ["None", "RITnet best_model", "RITnet Pupil", "RITnet Ellseg"]
+ritnet_ids = ["none", "ritnet_bestmodel", "ritnet_pupil", "ritnet_ellseg"]
+default_id = ritnet_ids[0]
